17c7a65c78cb15dd2db9505483bc9abb60a5dcd0
abdulkhaliquem9 <abdulkhaliquem9@gmail.com>
2022-04-13 01:57:39 +0530

    removed unnecessary code

--- src/Pagination/index.js
+++ src/Pagination/index.js
@@ -1,41 +1,26 @@
 import React, { useEffect, useState } from 'react'
-import mockData from './data.json';
 import './style.css'
-function Pagination() {
-    const PER_PAGE = 100;
-    const MAX_BUTTONS_TO_SHOW = 5
-    const totalRecords = mockData.length;
-    const [data, setData] = useState([])
+function Pagination({recordsPerPage = 60, totalRecords=1000, maxButtonsToShow = 5}) {
     const [currentPage, setCurrentPage] = useState(0)
     const [totalPages, setTotalPages] = useState(0)
-    const [pagesData, setPagesData] = useState({})
     const [visibleButtons, setVisibleButtons] = useState([])
     useEffect(() => {
         if (totalRecords > 0) {
-            const firstPageData = mockData.slice(0, PER_PAGE)
-            setData(mockData)
-            const totalPagesCount = Math.ceil(totalRecords / PER_PAGE)
+            const totalPagesCount = Math.ceil(totalRecords / recordsPerPage)
             setTotalPages(totalPagesCount)
-            const pagesDataCopy = {}
-            if (firstPageData.length > 0) {
+            if (totalRecords > 0) {
                 setCurrentPage(1)
-                for (let i = 1; i <= totalPagesCount; i++) {
-                    pagesDataCopy[i] = []
-                }
-                // setPagesData(pagesDataCopy)
-                setPagesData({ ...pagesDataCopy, '1': firstPageData })
             }
-            const pagesLength = Object.keys(pagesDataCopy).length
-            console.log('here', pagesDataCopy)
+            const pagesLength = totalPagesCount
             const pageButtons = []
-            if(pagesLength <= MAX_BUTTONS_TO_SHOW ){
+            if(pagesLength <= maxButtonsToShow ){
                 for(let i = 1; i<=pagesLength; i++)
                 {
                     pageButtons.push(i)
                 }
                 setVisibleButtons(pageButtons)
             }else{
-                 for(let i = 1; i<=5; i++)
+                for(let i = 1; i<=maxButtonsToShow; i++)
                 {
                     pageButtons.push(i)
                 }
@@ -43,10 +28,10 @@ function Pagination() {
             }
         }
     }, [])
-    console.log('data', visibleButtons)
+    // console.log('data', visibleButtons)
     const onNext = () => {
         let pageButtons = [...visibleButtons]
-        const buttonsCount = Object.keys(pagesData).length
+        const buttonsCount = totalPages
         const lastPageButtonNumber = (visibleButtons[visibleButtons.length-1])
         if(currentPage < buttonsCount){
             setCurrentPage(currentPage+1)
@@ -59,7 +44,7 @@ function Pagination() {
     }
     const onPrev = () => {
         let pageButtons = [...visibleButtons]
-        const buttonsCount = Object.keys(pagesData).length
+        const buttonsCount = totalPages
         const leastPageButtonNumber = (visibleButtons[0])
         if(currentPage > 1){
             setCurrentPage(currentPage-1)
@@ -72,18 +57,6 @@ function Pagination() {
     }
     return (
         <div className='pagination'>
-            pagination
-            {/* <div>
-                {
-                    Object.keys(pagesData).map((el, i) => <button key={el}
-                        className={currentPage === i + 1 ? 'active-button' : 'inactive-button'}
-                        onClick={() => { setCurrentPage(i + 1) }}
-                    >                        
-                    {el}
-                    </button>)
-                }
-            </div> */}
-
             <div>
                 <button onClick={onPrev}>{"<"}</button>
                 {
